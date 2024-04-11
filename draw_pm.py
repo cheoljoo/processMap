f189b53257eb7ae549b83707e2c50f6622e248d2
charles.lee <cheoljoo.lee@lge.com>
2024-04-11 10:47:50 +0900

    save input file into output directory

--- draw_pm.py
+++ draw_pm.py
@@ -939,8 +939,10 @@ skinparam usecase {
         totaltail += "```\n"
         if self.plantumlproxyserver:
             filename = self.outdir + '/' + '{id}-{infile}-total.md'.format(id=self.plantumlid,infile=self.input.split('/')[-1])
+            shutil.copy(self.input,os.path.join(self.outdir,'{id}-{infile}'.format(id=self.plantumlid,infile=self.input.split('/')[-1])))
         else:
             filename = self.outdir + '/' + 'total.md'.format(id=self.plantumlid,infile=self.input.split('/')[-1])
+            shutil.copy(self.input,os.path.join(self.outdir,'{infile}'.format(id=self.plantumlid,infile=self.input.split('/')[-1])))
         self.mdList.append(filename)
         print('write:',filename)
         f = open(filename,'w')
